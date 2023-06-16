eb1931c6670a1809461a294113de156a400a82c3
jeff <jeff.etale@student.moringaschool.com>
2023-06-16 11:16:59 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpBlock) {
+    const hqBlock = 42;
+
+    return Math.abs(pickUpBlock-hqBlock)
+    }
+ console.log(distanceFromHqInBlocks(34))
+
+ function distanceFromHqInFeet(pickUpBlock) {
+  let distanceInBlocks = distanceFromHqInBlocks(pickUpBlock)
+  let distanceInFeet = distanceInBlocks * 264;
+  return distanceInFeet
+ } 
+ console.log(distanceFromHqInFeet(34))
+
+ function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+    }
+ console.log(distanceTravelledInFeet(42,34))
+
+ function calculatesFarePrice(start, destination) {
+    let distanceTravelled = distanceTravelledInFeet(start, destination);
+    let farePrice;
+  
+    if (distanceTravelled <= 400) {
+      farePrice = 0;
+    } else if (distanceTravelled > 400 && distanceTravelled <= 2000) {
+      farePrice = (distanceTravelled - 400) * 0.02;
+    } else if (distanceTravelled > 2000 && distanceTravelled <= 2500) {
+      farePrice = 25;
+    } else {
+      farePrice = 'cannot travel that far';
+    }
+  
+    return farePrice;
+  }
+console.log(calculatesFarePrice(42,41))
+  
